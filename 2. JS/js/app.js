7a041fe10765e954c0a5c16c212aac7124afe2f2
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-07-10 11:29:51 +0300

    Implement signIn and askQuestion methods

--- 2. JS/js/app.js
+++ 2. JS/js/app.js
@@ -21,3 +21,65 @@ function Pupil(firstName, lastName) {
 }
 Pupil.prototype = Object.create(User.prototype);
 Pupil.prototype.constructor = Pupil;
+
+function signIn() {
+
+    // TODO: implement checkElementIsEmpty function
+
+    if (document.getElementById("teacher-first-name") != null)
+        var teacherFirstName = document.getElementById("teacher-first-name").value.trim();
+
+    if (document.getElementById("teacher-last-name") != null)
+        var teacherLastName = document.getElementById("teacher-last-name").value.trim();
+
+    if (teacherFirstName == "" || teacherLastName == "")
+    {
+        alert("Please Fill Teacher Name");
+        return false;
+    }
+
+    if (document.getElementById("pupil-first-name") != null)
+        var pupilFirstName = document.getElementById("pupil-first-name").value.trim();
+
+    if (document.getElementById("pupil-last-name") != null)
+        var pupilLastName = document.getElementById("pupil-last-name").value.trim();
+
+    if (pupilFirstName == "" || pupilLastName == "")
+    {
+        alert("Please Fill Pupil Name");
+        return false;
+    }
+
+    var teacher = new Teacher(teacherFirstName, teacherLastName);
+    var pupil = new Pupil(pupilFirstName, pupilLastName);
+    sessionStorage.setItem("teacherObject", JSON.stringify(teacher));
+    sessionStorage.setItem("pupilObject", JSON.stringify(pupil));
+    window.open("./askquestionpage.html","_self");
+} 
+
+function askQuestion() {
+ 
+    var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
+    var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
+
+    if (document.getElementById("textarea") != null)
+        var question = document.getElementById("textarea").value.trim();
+
+    if (question == "")
+        {
+            alert("Please Fill Question Field");
+            return false;
+        }
+    alert(question);
+}
+
+function setTextareaMode(checkbox) {
+    var textarea = document.getElementById("textarea");
+
+    if (checkbox.checked == true) {
+        textarea.readOnly = false;
+    }
+    else {
+        textarea.readOnly = true;
+    }
+}        
