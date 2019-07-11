50c2c91c2f9918395e906c95d4568a4184a8b1d5
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-07-11 13:07:20 +0300

    Fix "showing the collapsible content" action

--- 1. HTML & CSS/scripts/setPageStyle.js
+++ 1. HTML & CSS/scripts/setPageStyle.js
@@ -1,36 +1,35 @@
-<script type="text/javascript">
+var ul = document.getElementById("collapse-list-group");
+var selectedButton;
 
-    const backgroundColors = new Array('#3b3e4a','#ffffff');
-    const textColors = new Array('#ffffff', '#828282');
-    const borderColors = new Array('#3b3e4a', 'rgb(230, 230, 230)');
-    const {length} = backgroundColors
-    
-    
-    const setListItemStyle = (event) => {
-        let countPress = 0;
-        return function() {
-            el.parentNode.style.backgroundColor = backgroundColors[countPress];
-            el.parentNode.style.color = textColors[countPress];
-            el.parentNode.style.borderColor = borderColors[countPress];
-            switch (countPress) {
-                case 0:
-                    el.parentNode.style.borderBottom = 0;
-                    el.parentNode.style.borderRadius = '5px 5px 0px 0px';
-                    el.style.borderRadius = '0 5px 0 0';
-                    el.style.value = '-';
-                    break;
-                case 1:
-                    el.parentNode.style.borderBottom = '1px solid rgb(230, 230, 230)';
-                    el.parentNode.style.borderRadius = '5px';
-                    el.style.borderRadius = '5px';
-                    el.style.value = '+';
-                break;
-            }
-            countPress++;
-            if (countPress == backgroundColors.length) { 
-                countPress = 0; 
-            }
-        }
-    }     
+ul.onclick = function(event) {
+    var target = event.target;
 
-</script>
+    while (target != this) {
+      if (target.tagName == "BUTTON") {
+        highlight(target);
+        return;
+      }
+      target = target.parentNode;
+    }
+}
+
+function highlight(node) {
+    if (node.parentNode.style.backgroundColor == "rgb(59, 62, 74)") {
+        node.parentNode.style.backgroundColor = '#ffffff';
+        node.parentNode.style.color = '#828282';
+        node.parentNode.style.borderColor = 'rgb(230, 230, 230)';
+        node.parentNode.style.borderBottom = '1px solid rgb(230, 230, 230)';
+        node.parentNode.style.borderRadius = '5px';
+        node.style.borderRadius = '5px';
+        node.style.value = '+';
+    }
+    else {
+        node.parentNode.style.backgroundColor = '#3b3e4a';
+        node.parentNode.style.color = '#ffffff';
+        node.parentNode.style.borderColor = '#3b3e4a';
+        node.parentNode.style.borderBottom = 0;
+        node.parentNode.style.borderRadius = '5px 5px 0px 0px';
+        node.style.borderRadius = '0 5px 0 0';
+        node.style.value = '-';
+    }  
+}
