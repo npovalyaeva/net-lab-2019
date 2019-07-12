26feeb37b9526ff72d47f45ce98f4ce438c0b079
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-07-12 14:07:24 +0300

    Add "using of constructor functions" style of  inheritance

--- 2. JS/js/app.js
+++ 2. JS/js/app.js
@@ -2,34 +2,105 @@ function User(firstName, lastName) {
 
     this.firstName = firstName;
     this.lastName = lastName;
-}
 
-User.prototype.sayHi = function() {
-    alert( `Hello, i'm ${self.firstName} ${self.lastName}` );
-};
+    this.sayHi = function() {
+        alert( `Hello, i'm ${this.firstName} ${this.lastName}` );
+    };
+}
 
 function Teacher(firstName, lastName) {
 
-    User.call(this, firstName, lastName);
+    User.apply(this, arguments);
 
-    this.lastQuestion = null;
-    this.lastSetMark = -1;
+    var lastSetMark = -1;
+
+    this.getLastSetMark = function() {
+        return lastSetMark;
+    }
+
+    this.askQuestion = function(pupil, question) {
+        pupil.lastQuestion(question);
+    }
+
+    this.setMark = function(pupil, mark) {
+        if (pupil.marks(mark)) {
+            lastSetMark = mark;
+            pupil.lastAnswer(null);
+        }
+    }
 }
-Teacher.prototype = Object.create(User.prototype);
-Teacher.prototype.constructor = Teacher;
 
 function Pupil(firstName, lastName) {
 
-    User.call(this, firstName, lastName);
+    User.apply(this, arguments);
 
-    this.isAnswerForLastQuestionKnown = null;
-    this.isUnansweredQuestion = true; // variable is used to check if the "set mark" page can be loaded
-    this.lastAnswer = null;
-    this.marks = [];
+    var isAnswerForLastQuestionKnown = null;
+    var isUnansweredQuestion = true; // variable is used to check if the "set mark" page can be loaded
+    var lastQuestion = null;
+    var lastAnswer = null;
+    var marks = [];
 
+    this.isAnswerForLastQuestionKnown = function(flag) {
+        //getter
+        if (!arguments.length) return isAnswerForLastQuestionKnown;
+
+        //setter
+        // TODO: connect a library :)
+        if (typeof(flag) !== "boolean") {
+            alert("Please Input A Boolean Value");
+            return false;     
+        }
+        isAnswerForLastQuestionKnown = flag;
+    }
+
+    this.isUnansweredQuestion = function(flag) {
+        //getter
+        if (!arguments.length) return isUnansweredQuestion;
+
+        //setter
+        // TODO: connect a library :)
+        if (typeof(flag) !== "boolean") {
+            alert("Please Input A Boolean Value");
+            return false;     
+        }
+        isUnansweredQuestion = flag;
+    }
+
+    this.lastQuestion = function(question) {
+        //getter
+        if (!arguments.length) return lastQuestion;
+
+        //setter
+        lastQuestion = question;
+    }
+    
+    this.getLastAnswer = function() {
+        return lastAnswer;
+    }
+
+    this.marks = function(mark) {
+        // getter
+        if (!arguments.length) return marks;
+
+        // setter
+        if (isNaN(mark)) {
+            alert("Please Input A Number");
+            return false;
+        }
+        if (mark < 1 || mark > 10) {
+            alert("Please Input A Mark (1 <= Mark >= 10)");
+            return false;
+        }
+        marks.push(mark);
+        return true;
+    }
+
+    this.answerQuestion = function(isAnswerKnown, answer) {
+        this.isAnswerForLastQuestionKnown(isAnswerKnown);
+        lastAnswer = answer;
+        lastQuestion = null;
+    }
 }
-Pupil.prototype = Object.create(User.prototype);
-Pupil.prototype.constructor = Pupil;
 
 // --------------- --------------- --------------- --------------- --------------- --------------- ---------------
 
@@ -43,8 +114,8 @@ function checkIsSignedIn() {
 }
 
 function checkIsQuestionAsked() {
-    var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
-    if (teacher.lastQuestion == null) {
+    var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
+    if (pupil.lastQuestion() == null) {
         alert("You can't answer the question because the question isn't exist.");
         window.location = "askquestionpage.html";
         return false;
@@ -53,7 +124,7 @@ function checkIsQuestionAsked() {
 
 function checkIsQuestionAnswered() {
     var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
-    if (pupil.isUnansweredQuestion == true) {
+    if (pupil.isUnansweredQuestion() == true) {
         alert("You can't set a mark because the pupil hasn't answered the question yet.");
         window.location = "answerquestionpage.html";
         return false;
@@ -84,8 +155,8 @@ function signIn() {
 
     var teacher = new Teacher(teacherFirstName, teacherLastName);
     var pupil = new Pupil(pupilFirstName, pupilLastName);
-    sessionStorage.setItem("teacherObject", JSON.stringify(teacher));
-    sessionStorage.setItem("pupilObject", JSON.stringify(pupil));
+    sessionStorage.setItem("teacherObject", teacher);
+    sessionStorage.setItem("pupilObject", pupil);
     window.location = "askquestionpage.html";
 } 
 
@@ -93,27 +164,36 @@ function loadAskQuestionPage() {
 
     checkIsSignedIn();
 
-    var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
+    var teacherItem = sessionStorage.getItem("teacherObject");
+    var pupilItem = sessionStorage.getItem("pupilObject");
 
-    if (pupil.marks.length == 0) {
+    //!!!!!!!!!!!!!!!!!!!!!!!!
+    //var teacher = new Teacher(teacherItem.firstName, teacherItem.lastName);
+
+    if (pupil.marks().length == 0) {
         document.getElementById("marks-list").value = "You don't have marks!";
     }
     else {
-        document.getElementById("marks-list").value = pupil.marks.join(', ');
-
+        document.getElementById("marks-list").value = pupil.marks().join(', ');
     }
+
+    // Dumb thing to show sayHi() function working
+    teacher.sayHi();
+    pupil.sayHi();
 }   
 
 function askQuestion() {
     var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
     var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
 
-    teacher.lastQuestion = checkIsElementValueEmpty(document.getElementById("textarea"));
+    question = checkIsElementValueEmpty(document.getElementById("textarea"));
 
-    if (teacher.lastQuestion == "") {
+    if (question == "") {
         alert("Please Fill Question Field");
         return false;
     }
+
+    teacher.askQuestion(pupil, "");
 
     sessionStorage.setItem("teacherObject", JSON.stringify(teacher));
     sessionStorage.setItem("pupilObject", JSON.stringify(pupil));
@@ -127,9 +207,10 @@ function loadAnswerQuestionPage() {
     checkIsQuestionAsked();
 
     var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
+    var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
 
-    document.getElementById("last-set-mark").innerHTML = (teacher.lastSetMark == -1) ? "-" : teacher.lastSetMark; 
-    document.getElementById("question").innerHTML = teacher.lastQuestion;
+    document.getElementById("last-set-mark").innerHTML = (teacher.getLastSetMark() == -1) ? "-" : teacher.getLastSetMark(); 
+    document.getElementById("question").innerHTML = pupil.lastQuestion();
 }
 
 function setTextareaMode(checkbox) {
@@ -143,21 +224,22 @@ function setTextareaMode(checkbox) {
     }
 }   
 
+// !!!!!
 function answerQuestion() {
     var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
     var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
 
     var checkbox = document.getElementById("switch");
     if (checkbox.checked == true) {
-        pupil.isAnswerForLastQuestionKnown = true;
-        pupil.lastAnswer = document.getElementById("answer").value;
+        pupil.isAnswerForLastQuestionKnown() = true;
+        pupil.lastAnswer() = document.getElementById("answer").value;
     }
     else {
-        pupil.isAnswerForLastQuestionKnown = false;
+        pupil.isAnswerForLastQuestionKnown() = false;
     }
 
-    teacher.lastQuestion = null;
-    pupil.isUnansweredQuestion = false;
+    pupil.lastQuestion() = null;
+    pupil.isUnansweredQuestion() = false;
 
     sessionStorage.setItem("teacherObject", JSON.stringify(teacher));
     sessionStorage.setItem("pupilObject", JSON.stringify(pupil));
@@ -171,29 +253,18 @@ function loadSetMarkPage() {
 
     var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
     
-    document.getElementById("answer").innerHTML = (pupil.isAnswerForLastQuestionKnown == true) ? "Pupil answer is \"" + pupil.lastAnswer + "\"" : "The pupil doesn't know the answer";    
+    document.getElementById("answer").innerHTML = (pupil.isAnswerForLastQuestionKnown() == true) ? "Pupil answer is \"" + pupil.lastAnswer() + "\"" : "The pupil doesn't know the answer";    
 }
 
 function setMark() {
     var mark = checkIsElementValueEmpty(document.getElementById("mark"));
 
-    if (isNaN(mark)) {
-        alert("Please Input A Number");
-        return false;
-    }
-
-    if (mark < 1 || mark > 10) {
-        alert("Please Input A Mark (1 <= Mark >= 10)");
-        return false;
-    }
-
     var teacher = JSON.parse(sessionStorage.getItem("teacherObject"));
     var pupil = JSON.parse(sessionStorage.getItem("pupilObject"));
 
-    teacher.lastSetMark = mark;
-    pupil.lastAnswer = null;
-    pupil.isUnansweredQuestion = true;
-    pupil.marks.push(mark);
+    teacher.SetMark(mark);
+    pupil.lastAnswer() = null;
+    pupil.isUnansweredQuestion() = true;
 
     sessionStorage.setItem("teacherObject", JSON.stringify(teacher));
     sessionStorage.setItem("pupilObject", JSON.stringify(pupil));
