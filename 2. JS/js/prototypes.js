d999df30ebbbad062924ee13dbdf2824baefec78
npovalyaeva <iriejamaica@outlook.com>
2019-07-15 15:45:35 +0300

    Rename .js file and add the second realization

--- 2. JS/js/prototypes.js
+++ 2. JS/js/prototypes.js
@@ -1,114 +1,67 @@
 function User(firstName, lastName) {
-
     this.firstName = firstName;
     this.lastName = lastName;
-
-    this.sayHi = function() {
-        alert( `Hello, i'm ${this.firstName} ${this.lastName}` );
-    };
 }
 
+User.prototype.sayHi = function() {
+    alert( `Hello, i'm ${this.firstName} ${this.lastName}` );
+};
+
 function Teacher(firstName, lastName) {
-
     User.apply(this, arguments);
+    this.lastSetMark = -1;
+}
+Teacher.prototype = Object.create(User.prototype);
+Teacher.prototype.constructor = Teacher;
 
-    var lastSetMark = -1;
+Teacher.prototype.askQuestion = function(pupil, question) {
+    pupil.lastQuestion = question;
+}
 
-    this.getLastSetMark = function() {
-        if (lastSetMark == -1)
-            return false;
-        return lastSetMark;
-    }
-
-    this.askQuestion = function(pupil, question) {
-        pupil.lastQuestion(question);
-    }
-
-    this.setMark = function(pupil, mark) {
-        if (pupil.marks(mark)) {
-                lastSetMark = mark;
-                return true;
-        }
-        else{
+Teacher.prototype.setMark = function(pupil, mark) {
+    if (pupil.lastAnswer != null) {
+        if (isNaN(mark)) {
+            alert("Please Input A Number");
             return false;
         }
+        if (mark < 1 || mark > 10) {
+            alert("Please Input A Mark (1 <= Mark >= 10)");
+            return false;
+        }
+        pupil.marks.push(mark);
+        pupil.lastAnswer = null;
+        this.lastSetMark = mark;
+        return true;
     }
+    return false;
 }
 
 function Pupil(firstName, lastName) {
-
     User.apply(this, arguments);
+    this.isAnswerForLastQuestionKnown = null;
+    this.lastQuestion = null;
+    this.lastAnswer = null;
+    this.marks = [];
+}
+Pupil.prototype = Object.create(User.prototype);
+Pupil.prototype.constructor = Pupil;
 
-    var isAnswerForLastQuestionKnown = null;
-    var lastQuestion = null;
-    var lastAnswer = null;
-    var marks = [];
-
-    this.isAnswerForLastQuestionKnown = function(flag) {
-        //getter
-        if (!arguments.length) return isAnswerForLastQuestionKnown;
-
-        //setter
-        // TODO: connect a library :)
-        if (typeof(flag) !== "boolean") {
-            alert("Please Input A Boolean Value");
-            return false;     
-        }
-        isAnswerForLastQuestionKnown = flag;
-    }
-
-    this.lastQuestion = function(question) {
-        //getter
-        if (!arguments.length) return lastQuestion;
-
-        //setter
-        lastQuestion = question;
-    }
-    
-    this.getLastAnswer = function() {
-        return lastAnswer;
-    }
-
-    this.marks = function(mark) {
-        // getter
-        if (!arguments.length) return marks;
-
-        // setter
-        if (lastAnswer != null) {
-            if (isNaN(mark)) {
-                alert("Please Input A Number");
-                return false;
-            }
-            if (mark < 1 || mark > 10) {
-                alert("Please Input A Mark (1 <= Mark >= 10)");
-                return false;
-            }
-            marks.push(mark);
-            lastAnswer = null;
-            return true;
-        }
+Pupil.prototype.answerQuestion = function(isAnswerKnown, answer) {
+    if (this.lastQuestion != null) {
+        this.isAnswerForLastQuestionKnown = isAnswerKnown;
+        this.lastQuestion = null;
+        if (isAnswerKnown)
+            this.lastAnswer = answer;
         else
-            return false;
+            this.lastAnswer = "";
+        return true;
     }
-
-    this.answerQuestion = function(isAnswerKnown, answer) {
-        if (lastQuestion != null) {
-            this.isAnswerForLastQuestionKnown(isAnswerKnown);
-            lastQuestion = null;
-            if (isAnswerKnown)
-                lastAnswer = answer;
-            else 
-                lastAnswer = "";
-            return true;
-        }
-        return false;   
-    }
+    return false;
 }
 
 // --------------- --------------- --------------- --------------- --------------- --------------- ---------------
 
 function loadIndexPage() {
-    // TODO: Rewrite!
     document.getElementById("sign-in-page").style.display = "none";
     document.getElementById("ask-question-page").style.display = "none";
     document.getElementById("answer-question-page").style.display = "none";
@@ -122,7 +75,7 @@ function loadIndexPage() {
             loadSetMarkPage();
         }
         else {
-            if (window.pupil.lastQuestion() != null) {
+            if (window.pupil.lastQuestion != null) {
                 loadAnswerQuestionPage();
             }
             else {
@@ -157,21 +110,21 @@ function signIn() {
     window.teacher = new Teacher(teacherFirstName, teacherLastName);
     window.pupil = new Pupil(pupilFirstName, pupilLastName);
     loadIndexPage();
-} 
+}
 
 function loadAskQuestionPage() {
     document.getElementById("ask-question-page").style.display = "flex";
-    if (window.pupil.marks().length == 0) {
+    if (window.pupil.marks.length == 0) {
         document.getElementById("marks-list").value = "You don't have marks!";
     }
     else {
-        document.getElementById("marks-list").value = pupil.marks().join(', ');
+        document.getElementById("marks-list").value = pupil.marks.join(', ');
     }
 
     // A dumb thing to show sayHi() function working
     window.teacher.sayHi();
     window.pupil.sayHi();
-}   
+}
 
 function askQuestion() {
     question = checkIsElementValueEmpty(document.getElementById("textarea"));
@@ -188,8 +141,8 @@ function askQuestion() {
 function loadAnswerQuestionPage() {
     document.getElementById("answer-question-page").style.display = "flex";
 
-    document.getElementById("last-set-mark").innerHTML = (window.teacher.getLastSetMark() == false) ? "-" : window.teacher.getLastSetMark(); 
-    document.getElementById("question").innerHTML = window.pupil.lastQuestion();
+    document.getElementById("last-set-mark").innerHTML = (window.teacher.lastSetMark == -1) ? "-" : window.teacher.lastSetMark;
+    document.getElementById("question").innerHTML = window.pupil.lastQuestion;
 }
 
 function setTextareaMode(checkbox) {
@@ -201,7 +154,7 @@ function setTextareaMode(checkbox) {
     else {
         textarea.readOnly = true;
     }
-}   
+}
 
 function answerQuestion() {
     var checkbox = document.getElementById("switch");
@@ -218,9 +171,9 @@ function answerQuestion() {
 
 function loadSetMarkPage() {
     document.getElementById("set-mark-page").style.display = "flex";
-    
-    document.getElementById("pupil-answer-for-mark").innerHTML = (window.pupil.isAnswerForLastQuestionKnown() == true) ? 
-        "Pupil answer is \"" + window.pupil.getLastAnswer() + "\"" : "The pupil doesn't know the answer";    
+
+    document.getElementById("pupil-answer-for-mark").innerHTML = (window.pupil.isAnswerForLastQuestionKnown == true) ?
+        "Pupil answer is \"" + window.pupil.lastAnswer + "\"" : "The pupil doesn't know the answer";
 }
 
 function setMark() {
