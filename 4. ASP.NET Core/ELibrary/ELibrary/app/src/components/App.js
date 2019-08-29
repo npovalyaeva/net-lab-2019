aaa9e9bc8299b6071241ba8fa864c5047e06108d
npovalyaeva <iriejamaica@outlook.com>
2019-08-29 14:55:37 +0300

    Start to create Authorization page

--- 4. ASP.NET Core/ELibrary/ELibrary/app/src/components/App.js
+++ 4. ASP.NET Core/ELibrary/ELibrary/app/src/components/App.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import '../styles/App.css';
 
+import TextField from '@material-ui/core/TextField';
 
 import { Header } from './Header';
 import { Footer } from './Footer';
-
-import wallpaper from '../sources/wallpaper.jpg';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <img
-          src={wallpaper}
-          width="100%"
-          className="main-image"
-          alt="Main image"
-      />
-
+      <div className="main-content">
+        <div className="quote">
+          <h3>“Reading brings us unknown friends.”</h3>
+          <h5>Honoré de Balzac</h5>
+        </div>
+        <div className="registration-form">
+          <TextField
+            label="First Name"
+          />
+          <TextField
+            label="Last Name"
+          />
+        </div>
+      </div>
       <Footer/>
     </div>
   );
--- 4. ASP.NET Core/ELibrary/ELibrary/app/src/components/Header.js
+++ 4. ASP.NET Core/ELibrary/ELibrary/app/src/components/Header.js
@@ -3,7 +3,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
 
+import '../styles/Header.css';
+
 import logo from '../sources/logo.svg';
+
 
 export class Header extends PureComponent {
 
@@ -12,14 +15,15 @@ export class Header extends PureComponent {
             <AppBar className="app-bar" position="static">
                 <Toolbar className="toolbar">
                     <a href="#">
-                        <img
-                            src={logo}
-                            width="50"
-                            height="50"
-                            className="main-logo"
-                            alt="SSAW Library logo"
-                        />
+                    <img
+                        src={logo}
+                        width="70"
+                        height="70"
+                        className="main-logo"
+                        alt="SSAW Library"
+                    />
                     </a>
+                    <h2 className="main-name">Bookshelf</h2>
                     <Button className="login-button" color="inherit">Login</Button>
                 </Toolbar>
             </AppBar>
