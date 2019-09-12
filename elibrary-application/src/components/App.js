0a4f71fa18e745aca1100b3ae7abbdccd9b2a481
npovalyaeva <iriejamaica@outlook.com>
2019-09-12 14:16:27 +0300

    Add registration form

--- elibrary-application/src/components/App.js
+++ elibrary-application/src/components/App.js
@@ -1,34 +1,57 @@
 import React, { PureComponent } from 'react';
 
-import TextField from '@material-ui/core/TextField';
+import { Button, TextField } from '@material-ui/core';
 
 class App extends PureComponent {
     render() {
         return (
             <React.Fragment>
-                <TextField
-                    id="standard-email-input"
-                    label="Email"
-                    className="email-input"
-                    type="email"
-                    name="email"
-                    autoComplete="email"
-                    margin="normal"
-                />
-                <TextField
-                  id="standard-password-input"
-                  label="Password"
-                  className="password-input"
-                  type="password"
-                  margin="normal"
-                />
-                <TextField
-                  id="standard-password-input"
-                  label="Repeat Password"
-                  className="repeat-password-input"
-                  type="password"
-                  margin="normal"
-                />
+                <form className="registration" noValidate autoComplete="on">
+                    <TextField
+                        id="standard-name"
+                        label="First Name"
+                        className="first-name-input"
+                        margin="normal"
+                    />
+                    <TextField
+                        id="standard-name"
+                        label="Last Name"
+                        className="last-name-input"
+                        margin="normal"
+                    />
+                    <TextField
+                        id="standard-email-input"
+                        label="Username"
+                        className="username-input"
+                        margin="normal"
+                    />
+                    <TextField
+                        id="standard-email-input"
+                        label="Email"
+                        className="email-input"
+                        type="email"
+                        name="email"
+                        autoComplete="email"
+                        margin="normal"
+                    />
+                    <TextField
+                      id="standard-password-input"
+                      label="Password"
+                      className="password-input"
+                      type="password"
+                      margin="normal"
+                    />
+                    <TextField
+                      id="standard-password-input"
+                      label="Repeat Password"
+                      className="repeat-password-input"
+                      type="password"
+                      margin="normal"
+                    />
+                    <Button variant="contained" color="primary" className="sign-up-button">
+                        Sign Up
+                    </Button>
+                </form>
             </React.Fragment>
         )
     }
--- elibrary-application/src/index.js
+++ elibrary-application/src/index.js
@@ -19,7 +19,7 @@ const store = configureStore();
 
 render(
     <Provider store={store}>
-        <App />,
+        <App />
     </Provider>,
     document.getElementById('root')
 );
