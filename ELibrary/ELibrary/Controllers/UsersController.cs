3d9aa1a6dd383453be4f8ae50526300d0e40712c
npovalyaeva <iriejamaica@outlook.com>
2019-09-13 14:27:32 +0300

    Update UsersController.cs

--- ELibrary/ELibrary/Controllers/UsersController.cs
+++ ELibrary/ELibrary/Controllers/UsersController.cs
@@ -37,7 +37,7 @@ namespace ELibrary.Controllers
 
         // GET: API/Users/Login
         [HttpGet("login")]
-        public async Task<IActionResult> Authenticate([FromBody] AuthenticationModel authenticationData)
+        public async Task<IActionResult> Authenticate(AuthenticationModel authenticationData)
         {
             var user = await _userService.Authenticate(authenticationData);
             if (user == null)
@@ -47,114 +47,72 @@ namespace ELibrary.Controllers
             return Ok(user);
         }
 
+        // GET: API/Users/Checking
+        [HttpGet("checking/{login}")]
+        public async Task<IActionResult> CheckLogin(string login)
+        {
+            var loginModel = await _userService.CheckIsLoginUnique(login);
+            return Ok(loginModel);
+        }
 
+        // GET: API/Users/Details/5
+        [HttpGet("details/{id}")]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var user = await _userService.GetUserInfo((int)id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return Ok(user);
+        }
 
+        // POST: API/Users
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserModel user)
+        {
+            var userModel = await _userService.Create(user);
+            if (userModel == null)
+            {
+                return BadRequest();
+            }
+            //              ??? 
+            return Created("Created", userModel);
+        }
 
+        // PUT: API/Users/Block/5
 
+        // TODO: Review
 
+        [HttpPut("block")]
+        public async Task<IActionResult> Block(BlockUserModel user)
+        {
+            var userModel = await _userService.Block(user);
+            if (userModel == null)
+            {
+                return BadRequest();
+            }
+            return Created("Updated", userModel);
+        }
 
-
-
-
-        //// GET: Users/Checking
-        //[HttpGet("checking/{login}")]
-        //public async Task<IActionResult> CheckLogin(string login)
-        //{
-        //    LoginModel loginModel = new LoginModel();
-        //    loginModel.Login = login;
-
-        //    var user = await _context.User
-        //        .Where(m => m.Email == login || m.Username == login)
-        //        .FirstOrDefaultAsync();
-
-        //    loginModel.IsUnique = (user == null) ? true : false;
-
-        //    return Ok(loginModel);
-        //}
-
-
-        //// GET: Users/Details/5
-        //[HttpGet("details/{id}")]
-        //public async Task<IActionResult> Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    var user = await _context.User
-        //        .FirstOrDefaultAsync(m => m.UserId == id);
-
-        //    if (user == null)
-        //    {
-        //        return NotFound();
-        //    }
-
-        //    return Ok(_mapper.Map<User, UserModel>(user));
-        //}
-
-        //// POST: Users
-        //[HttpPost]
-        //public async Task<IActionResult> Create([FromBody] CreateUserModel user)
-        //{
-        //    var dbObject = _mapper.Map<CreateUserModel, User>(user);
-
-        //    dbObject.PasswordHash = Hash.FindHash(user.Password);
-        //    dbObject.RoleId = 1;
-        //    dbObject.IsBlocked = false;
-
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(dbObject);
-        //        await _context.SaveChangesAsync();
-        //    }
-        //    return CreatedAtAction(nameof(Details), new { id = dbObject.UserId }, _mapper.Map<User, SuccessUserModel>(dbObject));
-        //}
-
-        //// PUT: Users/Block/5
-        //[HttpPut("block")]
-        //public async Task<IActionResult> Block([FromBody] BlockUserModel user)
-        //{
-        //    var dbObject = await _context.User
-        //        .FirstOrDefaultAsync(m => m.UserId == user.UserId);
-
-        //    dbObject.BlockedReason = user.BlockingReason;
-        //    dbObject.IsBlocked = true;
-        //    _context.User.Update(dbObject);
-
-        //    var reservations = await _context.Reservation
-        //        .Where(m => m.UserId == user.UserId)
-        //        .ToListAsync();
-        //    foreach (var reservation in reservations)
-        //    {
-        //        var book = await _context.Book
-        //            .FirstOrDefaultAsync(m => m.BookId == reservation.BookId);
-        //        book.FreeCopiesCount++;
-        //        _context.Book.Update(book);
-        //        _context.Reservation.Remove(reservation);
-        //    }
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction(nameof(Details), new { id = dbObject.UserId }, _mapper.Map<User, UserBlockingStatusModel>(dbObject));
-        //}
-
-        //// PUT: Users/Unblock/5
-        //[HttpPut("unblock/{id}")]
-        //public async Task<IActionResult> Unblock(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    var dbObject = await _context.User
-        //        .FirstOrDefaultAsync(m => m.UserId == id);
-            
-        //    dbObject.BlockedReason = null;
-        //    dbObject.IsBlocked = false;
-        //    _context.User.Update(dbObject);
-        //    await _context.SaveChangesAsync();
-
-        //    return CreatedAtAction(nameof(Details), new { id = dbObject.UserId }, _mapper.Map<User, UserBlockingStatusModel>(dbObject));
-        //}
+        // PUT: Users/Unblock/5
+        [HttpPut("unblock/{id}")]
+        public async Task<IActionResult> Unblock(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var userModel = await _userService.Unblock((int)id);
+            if (userModel == null)
+            {
+                return BadRequest();
+            }
+            return Created("Updated", userModel);
+        }
     }
 }
