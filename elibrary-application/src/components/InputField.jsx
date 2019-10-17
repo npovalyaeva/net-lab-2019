5685c6f2bae0f775ceba11f0855b02697c086d49
Nadya Povalyaeva <iriejamaica@outlook.com>
2019-10-17 12:00:57 +0300

    Add opportunity for validation in Input Component

--- elibrary-application/src/components/InputField.jsx
+++ elibrary-application/src/components/InputField.jsx
@@ -1,35 +1,44 @@
 import React from 'react';
 
 import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import FormHelperText from '@material-ui/core/FormHelperText';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
 
-const InputField = ({ input, defaultValue, name, label, value, type, onChange, meta: { touched, error, warning } }) => (
-
-    // <div className="inputBlock">
-    //     <h3>{label}</h3>
-    //     <div>
-    //         <input {...input}
-    //             placeholder={defaultValue}
-    //             type={type}
-    //         />
-    //         {touched &&
-    //             ((error && <span>{error}</span>) ||
-    //             (warning && <span>{warning}</span>))}
-    //     </div>
-    // </div>
-    <div>
-        <TextField {...input}
-            //onChande={onChange}
-            label={label}
-            value={defaultValue}
-            type={type}
-            className="inputBlock"
-            margin="normal"
-        />
-        {touched &&
-            ((error && <span>{error}</span>) ||
-            (warning && <span>{warning}</span>))
+const InputField = ({ input, defaultValue, label, type, errorMessage }) => (
+    (() => {
+        if (typeof errorMessage === 'undefined') {
+            return (
+                <FormControl className={label}>
+                    <InputLabel htmlFor={label}>{label}</InputLabel>
+                    <Input {...input}
+                        id={label}
+                        type={type}
+                        value={defaultValue}
+                        aria-describedby="component-error-text"
+                    />
+                </FormControl>
+            )
         }
-    </div>
+        else {
+            return (
+                <FormControl className={label} error>
+                    <InputLabel htmlFor={label}>{label}</InputLabel>
+                    <Input {...input}
+                        id={label}
+                        type={type}
+                        value={defaultValue}
+                        aria-describedby="component-error-text"
+                    />
+                    <FormHelperText id="component-error-text">{errorMessage}</FormHelperText>
+                </FormControl>
+            )
+        }
+        
+    })()
+    
 )
 
 export default InputField;
+
